07787331b84b148210dbfbb545b845f3ec6c79ea
minh.pn <minh.pn811@gmail.com>
2021-12-21 00:05:57 +0700

    Update new class controller

--- controllers/get_data.js
+++ controllers/get_data.js
@@ -1,5 +1,31 @@
-// list coin
+// init state
 
+const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko();
+
+
+class GetCKCData  {
+    async ping() {
+        let data = await CoinGeckoClient.ping();
+        console.log(data);
+    };
+
+    async getListCoin() {
+        let data = await CoinGeckoClient.coins.list();
+        console.log(data);
+    };
+
+    async sendNotification() {
+        try {
+            let data = await CoinGeckoClient.coins.list();
+            console.log(data);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+GetCKCData.sendNotification();
 
 // detail coin
 
