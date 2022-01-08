26c62a819b4d66a0400f192c6b535cb81e781bb2
minh.pn <minh.pn811@gmail.com>
2022-01-09 02:00:05 +0700

    Completed deriavites controller

--- controllers/derivatives.controller.js
+++ controllers/derivatives.controller.js
@@ -11,6 +11,7 @@ const { formatDataFunc, clean } = require("../utils/utils")
 class CGKDerivativeController {
 	constructor() {
 		this.derivative_tickers = fs.existsSync('data/derivative/derivative_tickers.json') ? fs.readFileSync('data/derivative/derivative_tickers.json') : '[]';
+		this.derivative_exchanges_list = fs.existsSync('data/derivative/derivative_exchange_list.json') ? fs.readFileSync('data/derivative/derivative_exchange_list.json') : '[]';
 		this.derivative_tickers_index = "cgk_derivative_tickers"
 		this.derivative_exchanges_index = "cgk_derivative_exchanges"
 		// this.exchange_detail_index = "cgk_exchange_detail"
@@ -47,7 +48,7 @@ class CGKDerivativeController {
 			let insert_data = JSON.parse(this.derivative_tickers.toString())
 			insert_data = insert_data.filter(item => clean(item));
 			//console.log(insert_data);
-			if (insert_data.length) await elasticService.create_bulk(this.derivative_tickers_index, insert_data)
+			await elasticService.create_bulk(this.derivative_tickers_index, insert_data)
 			//elasticService.check_health();
 		} catch (e) {
 			console.log(e)
@@ -57,11 +58,68 @@ class CGKDerivativeController {
 		return true
 	}
 
-	// get derivative exchanges
-	async fetchDerivativeExchanges() {
-
+	// get derivative exchanges list (id)
+	async fetchDerivativeExchangesList() {
+		try {
+			const data = await CoinGeckoClient.derivatives.listExchanges()
+			fs.writeFileSync('data/derivative/derivative_exchange_list.json', JSON.stringify(data.data))
+			console.log('total exchange list are ' + data.data.length + ' elements.')
+			
+		} catch (e) {
+			console.log(e)
+			return false
+		}
+		return true
 	}
-
+	
+	// get derivative exchange data by id
+	async fetchDerivativeExchangeData (id, params = {}) {
+		try {
+			params.tickers = true
+			await new Promise(resolve => setTimeout(resolve, 5/6*1000))
+			let response = await CoinGeckoClient.derivatives.fetchExchange(id, params)
+			// console.log(response.data)
+			return clean(response.data)
+		} catch (error) {
+			console.log(error)
+			return false;
+		}
+	}
+	
+	// get all derivative exchange (paginate)
+	async fetchDerivativeAllExchange (params = {}) {
+		try {
+			await new Promise(resolve => setTimeout(resolve, 5/6*1000))
+			let response = await CoinGeckoClient.derivatives.allExchanges(params)
+			//console.log(response.data)
+			return response.data.filter(item => clean(item)); // array
+		} catch (error) {
+			console.log(error)
+			return false;
+		}
+	}
+	
+	async syncDerivativeAllExchange () {
+		try {
+			let params = {
+				per_page: 100,
+				page: 1
+			}
+			let response = await this.fetchDerivativeAllExchange(params);
+			
+			// while(response.length) {
+			// 	params.page ++;
+			// 	await this.syncDerivativeAllExchange();
+			// }
+			
+			await elasticService.create_bulk(this.derivative_exchanges_index, response)
+			
+		} catch (error) {
+			console.log(error)
+			return false;
+		}
+	}
+	
 
 	
 }
