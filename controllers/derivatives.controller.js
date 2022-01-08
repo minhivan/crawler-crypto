a7b91c6bb955c82da4d522893ac659921896c9b5
minh.pn <minh.pn811@gmail.com>
2022-01-08 18:41:41 +0700

    Update derivative controller

--- controllers/derivatives.controller.js
+++ controllers/derivatives.controller.js
@@ -5,54 +5,64 @@ const CoinGeckoClient = new CoinGecko()
 const fs = require('fs')
 // implement elasticsearch
 const elasticService = require("../services/elastic.service")
-const { formatDataFunc, testFormatCoinData } = require("../utils/utils")
+const { formatDataFunc, clean } = require("../utils/utils")
 
 
 class CGKDerivativeController {
 	constructor() {
-	
+		this.derivative_tickers = fs.existsSync('data/derivative/derivative_tickers.json') ? fs.readFileSync('data/derivative/derivative_tickers.json') : '[]';
+		this.derivative_tickers_index = "cgk_derivative_tickers"
+		this.derivative_exchanges_index = "cgk_derivative_exchanges"
+		// this.exchange_detail_index = "cgk_exchange_detail"
 	}
 	
 	async ping() {
 		let data = await CoinGeckoClient.ping();
 		console.log(data);
 	};
-	
+
+
 	// // get data and store it to json file
-	// async fetchCoinList() {
-	// 	try {
-	// 		const data = await CoinGeckoClient.coins.list()
-	// 		if (data.data.length) {
-	// 			fs.writeFileSync('data/coin_list.json', JSON.stringify(data.data))
-	// 			console.log('total coin are ' + data.data.length + ' elements.')
-	// 		}
-	//
-	// 	} catch (e) {
-	// 		console.log(e)
-	// 		return false
-	// 	}
-	// 	return true
-	// }
-	//
-	// // sync data to elastic
-	// async syncCoinList() {
-	// 	try {
-	// 		// map data and insert to db
-	// 		console.log("Syncing data to elastic")
-	// 		let insert_data = JSON.parse(this.coin_list.toString())
-	// 		insert_data = insert_data.filter(item => {
-	// 			return formatDataFunc('coin_list', item)
-	// 		});
-	// 		//console.log(insert_data);
-	// 		if (insert_data.length) await elasticService.create_bulk(this.coin_list_index, insert_data)
-	// 		//elasticService.check_health();
-	// 	} catch (e) {
-	// 		console.log(e)
-	// 		return false
-	// 		//write log here
-	// 	}
-	// 	return true
-	// }
+	async fetchDerivativeTickers() {
+		try {
+			const data = await CoinGeckoClient.derivatives.fetchTickers()
+			if (data.data.length) {
+				fs.writeFileSync('data/derivative/derivative_tickers.json', JSON.stringify(data.data))
+				console.log('total coin are ' + data.data.length + ' elements.')
+			}
+
+		} catch (e) {
+			console.log(e)
+			return false
+		}
+		return true
+	}
+
+	// sync data to elastic
+	async syncDerivativeTickers() {
+		try {
+			await this.fetchDerivativeTickers;
+			// map data and insert to db
+			console.log("Syncing data to elastic")
+			let insert_data = JSON.parse(this.derivative_tickers.toString())
+			insert_data = insert_data.filter(item => clean(item));
+			//console.log(insert_data);
+			if (insert_data.length) await elasticService.create_bulk(this.derivative_tickers_index, insert_data)
+			//elasticService.check_health();
+		} catch (e) {
+			console.log(e)
+			return false
+			//write log here
+		}
+		return true
+	}
+
+	// get derivative exchanges
+	async fetchDerivativeExchanges() {
+
+	}
+
+
 	
 }
 
