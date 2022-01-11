dde5b9e08bce7f0cad043d9e8eea8b820ee569b0
minh.pn <minh.pn811@gmail.com>
2022-01-11 12:11:26 +0700

    Update fprocess split

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ let CGKCoin = require('./controllers/coin.controller');
 let CGKExchange = require('./controllers/exchange.controller')
 let CGKDerivative = require('./controllers/derivatives.controller')
 
-
-
 // init fetch all data list & create files
 const init = async () => {
     // fetch coin list
     await CGKCoin.syncCoinList();
 
     // fetch exchange list
-    await CGKExchange.syncListExchange();
+    //await CGKExchange.syncListExchange();
 
     // fetch derivatives list
-    await CGKDerivative.fetchDerivativeExchangesList()
+    //await CGKDerivative.fetchDerivativeExchangesList()
 
 }
 
@@ -21,25 +19,36 @@ const init = async () => {
 process.argv.forEach(async function (val, index, array) {
     switch (val) {
         case "sync_coin_detail":
-            await CGKCoin.syncBatchCoinDetails();
-            await new Promise(resolve => setTimeout(resolve, 600000*6))
+            await CGKCoin.syncBatchCoinDetails(25);
             break;
+
+        case "sync_coin_market":
+            await CGKCoin.syncCoinMarket(50);
+            // await new Promise(resolve => setTimeout(resolve, 1000*60*15))
+            break;
+
         case "sync_exchange_detail":
-            await CGKExchange.syncBatchExchangeDetails();
+            await CGKExchange.syncBatchExchangeDetails(50);
+            // await new Promise(resolve => setTimeout(resolve, 1000*60*15))
             break;
+
         case "sync_derivative_detail":
             await CGKDerivative.syncDerivativeAllExchange();
             break;
-        case "exchange_rates":
+
+        case "sync_exchange_rates":
             await CGKExchange.syncExchangeRates();
             break;
-        case "coin_market":
-            await CGKCoin.syncCoinMarket(250);
-            await new Promise(resolve => setTimeout(resolve, 600000*6))
+
+        case "test":
+            console.log("asdadasdad");
             break;
+
+
         case "init":
             await init();
     }
+    await new Promise(resolve => setTimeout(resolve, 1000*3)) // wait for another function setup
 });
 
 
