810ac2cb54c7684c3efc350923a90cd170edfd8e
minh.pn <minh.pn811@gmail.com>
2022-01-11 17:35:48 +0700

    Update index function

--- index.js
+++ index.js
@@ -8,49 +8,13 @@ const init = async () => {
     await CGKCoin.syncCoinList();
 
     // fetch exchange list
-    //await CGKExchange.syncListExchange();
+    await CGKExchange.syncListExchange();
 
     // fetch derivatives list
     //await CGKDerivative.fetchDerivativeExchangesList()
 
 }
 
-
-// process.argv.forEach(async function (val, index, array) {
-//     switch (val) {
-//         case "sync_coin_detail":
-//             await CGKCoin.syncBatchCoinDetails(25);
-//             break;
-//
-//         case "sync_coin_market":
-//             await CGKCoin.syncCoinMarket(50);
-//             await new Promise(resolve => setTimeout(resolve, 1000*3)) // wait for another function setup
-//             break;
-//
-//         case "sync_exchange_detail":
-//             await CGKExchange.syncBatchExchangeDetails(50);
-//             await new Promise(resolve => setTimeout(resolve, 1000*3)) // wait for another function setup
-//             break;
-//
-//         case "sync_derivative_detail":
-//             await CGKDerivative.syncDerivativeAllExchange();
-//             break;
-//
-//         case "sync_exchange_rates":
-//             await CGKExchange.syncExchangeRates();
-//             await new Promise(resolve => setTimeout(resolve, 1000*3)) // wait for another function setup
-//
-//             break;
-//
-//         case "test":
-//             console.log("asdadasdad");
-//             break;
-//
-//
-//         case "init":
-//             await init();
-//     }
-// });
 
 
 (async function () {
@@ -71,15 +35,16 @@ const init = async () => {
                 await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
                 break;
 
+            case "sync_exchange_rates":
+                await CGKExchange.syncExchangeRates();
+                await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
+                break;
+
             case "sync_derivative_detail":
                 await CGKDerivative.syncDerivativeAllExchange();
                 await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
                 break;
 
-            case "sync_exchange_rates":
-                await CGKExchange.syncExchangeRates();
-                await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
-                break;
 
             case "test":
                 console.log("Test case 1");
@@ -92,5 +57,7 @@ const init = async () => {
         }
     }
 })()
+
+
 
 
