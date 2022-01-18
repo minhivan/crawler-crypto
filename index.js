357dd6bdfdce9670cad3ac72b54a010473b3bd2e
minh.pn <minh.pn811@gmail.com>
2022-01-18 15:39:37 +0700

    Update index func

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const init = async () => {
     // fetch coin list
     await CGKCoin.fetchCoinList();
     // fetch exchange list
-   // await CGKExchange.syncListExchange();
+    // await CGKExchange.syncListExchange();
 
 }
 
@@ -18,43 +18,42 @@ const init = async () => {
         switch (val) {
             case "sync_coin_detail": // Loop coin list
                 await CGKCoin.syncBatchCoinDetails(25);
-                await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
+                await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
                 break;
 
             case "sync_coin_market":
                 await CGKCoin.syncCoinMarket(50);
-                await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
+                await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
                 break;
-            
+
             case "sync_coin_ticker": // Loop coin list
                 await CGKCoin.syncCoinTickers();
-                await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
+                await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
                 break;
-            
-                
-            // case "sync_coin_chart":
-            //     await CGKExchange.syncBatchExchangeDetails(50);
-            //     await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
-            //     break;
+
+            case "sync_coin_market_chart":
+                await CGKCoin.syncCoinMarketChart(50, 1);
+                await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
+                break;
 
             case "sync_exchange_detail":
                 await CGKExchange.syncBatchExchangeDetails(50);
-                await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
+                await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
                 break;
 
             case "sync_exchange_ticker": // Loop exchange list
                 await CGKExchange.syncExchangeAllTickers();
-                await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
+                await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
                 break;
 
             case "sync_exchange_rates":
                 await CGKExchange.syncExchangeRates();
-                await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
+                await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
                 break;
 
             case "sync_derivative_exchange":
                 await CGKDerivative.syncDerivativeAllExchange();
-                await new Promise(resolve => setTimeout(resolve, 1000*60*15)) // wait for another function setup
+                await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 15)) // wait for another function setup
                 break;
 
             case "test":
